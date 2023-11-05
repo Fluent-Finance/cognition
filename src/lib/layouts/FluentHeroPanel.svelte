7c760a756ce93421d97987d70d04291b3c47f7d4
⚡️0x0O0O0⚡️ <757859+0xO0O0@users.noreply.github.com>
2023-11-05 17:01:40 -0500

    dev: redesign hero panel, added svg icons

--- src/lib/layouts/FluentHeroPanel.svelte
+++ src/lib/layouts/FluentHeroPanel.svelte
@@ -1,18 +1,24 @@
 <!-- handcoded with ♥︎  by ⚡️-𝙆𝙊𝘿𝞝𝙋𝞸𝞝𝙏-⚡️--->
 <script lang='ts'>
+  import Inoutcircle from '$lib/utils/icons/Inoutcircle.svelte';
+  import Fingerprint from '$lib/utils/icons/Fingerprint.svelte';
+  import Clipboard   from '$lib/utils/icons/Clipboard.svelte';
+
+  export let classes = 'mx-auto mb-4 w-15 h-15 text-primary-600 dark:text-primary-500';
+
   let pclasses = `
-    font-light
+    font-normal 
     font-neuehass
     leading-loose
     text-center
     align-center
     my-10
-    mx-40
-    px-30
-    text-xl 
-    md:text-xl 
-    lg:text-2xl
-    xl:text-3xl
+    mx-20
+    px-20
+    text-lg
+    md:text-lg  
+    lg:text-lg 
+    xl:text-xl
     text-gray-500
     dark:text-gray-400
   `;
@@ -20,7 +26,7 @@
   let h1classes = `
     font-basier 
     font-semibold
-    my-20
+    my-0
     mx-20
     px-40
     max-w-full
@@ -48,9 +54,9 @@
 </script> 
 
 <section class="bg-white dark:bg-transparent">
-  <div id="anchor-learnmore" class="bg-neutral rounded-2xl py-8 px-15 mx-15 min-h-screen max-w-screen-xl text-center sm:py-16 lg:px-6">
+  <div id="anchor-learnmore" class="bg-neutral rounded-2xl py-8 px-15 mx-15 min-h-fit max-w-screen-xl text-center sm:py-16 lg:px-6">
 
-    <div class="flex w-xl mx-30">
+    <div class="flex w-xl py-20 mx-30">
       <h1 class={h1classes}>
         Interoperable <span class={onchain}>Infrastructure</span>
         for a Fragmented Financial System
@@ -71,19 +77,21 @@
         enables banks and FIs to enter the on-chain world
       </h2> 
     </div>
-    <div class="my-10 lg:my-12 space-y-8 md:grid md:grid-cols-3 md:gap-12 md:space-y-0">
-      <div>
-        <svg class="mx-auto mb-4 w-15 h-15 text-primary-600 dark:text-primary-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.625 2.655A9 9 0 0119 11a1 1 0 11-2 0 7 7 0 00-9.625-6.492 1 1 0 11-.75-1.853zM4.662 4.959A1 1 0 014.75 6.37 6.97 6.97 0 003 11a1 1 0 11-2 0 8.97 8.97 0 012.25-5.953 1 1 0 011.412-.088z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M5 11a5 5 0 1110 0 1 1 0 11-2 0 3 3 0 10-6 0c0 1.677-.345 3.276-.968 4.729a1 1 0 11-1.838-.789A9.964 9.964 0 005 11zm8.921 2.012a1 1 0 01.831 1.145 19.86 19.86 0 01-.545 2.436 1 1 0 11-1.92-.558c.207-.713.371-1.445.49-2.192a1 1 0 011.144-.83z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M10 10a1 1 0 011 1c0 2.236-.46 4.368-1.29 6.304a1 1 0 01-1.838-.789A13.952 13.952 0 009 11a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-        <h3 class="mb-2 text-xl font-bold dark:text-white">Tokenize Off-Chain Assets</h3>
 
-      </div>
-      <div>
-        <svg class="mx-auto mb-4 w-15 h-15 text-primary-600 dark:text-primary-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>
-        <h3 class="mb-2 text-xl font-bold dark:text-white">Leverage Off-Chain Collateral</h3>
-      </div>
-      <div>
-        <svg class="mx-auto mb-4 w-15 h-15 text-primary-600 dark:text-primary-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-        <h3 class="mb-2 text-xl font-bold dark:text-white">Issue & Redeem Instantly</h3>
+    <div class="flex w-xl mt-10 px-20 mx-80">
+      <div class="my-10 lg:my-12 space-y-8 md:grid md:grid-cols-3 md:gap-12 md:space-y-0">
+        <div>
+          <Fingerprint class={classes} />
+          <h3 class="mb-2 text-lg font-bold dark:text-white">Tokenize Off-Chain Assets</h3>
+        </div>
+        <div>
+          <Clipboard class={classes} />
+          <h3 class="mb-2 text-xl font-bold dark:text-white">Leverage Off-Chain Collateral</h3>
+        </div>
+        <div>
+          <Inoutcircle class={classes} />
+          <h3 class="mb-2 text-xl font-bold dark:text-white">Issue & Redeem Instantly</h3>
+        </div>
       </div>
     </div>
   </div>
---
+++ src/lib/utils/icons/Fingerprint.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let size = '61';
+  export let color = '#6200EE';
+</script>
+
+<svg aria-label="fingerprint" role="img" width={size} height={size} fill='none' class={$$props.class} viewBox="0 0 61 61" xmlns="http://www.w3.org/2000/svg">
+<g id="Group 34">
+<path id="Vector 7" d="M44.6283 59.8047H59.5V44.933" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Vector 6" d="M16.3717 59.8047H1.5V44.933" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Vector 5" d="M44.6283 1.80469H59.5V16.6764" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Vector 8" d="M16.3717 1.80469H1.5V16.6764" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Ellipse 1" d="M50.5774 41.9583V27.8302C50.5774 16.7421 41.5887 7.75342 30.5006 7.75342C19.4125 7.75342 10.4238 16.7421 10.4238 27.8302V41.9583" stroke={color} stroke-width="2" stroke-linecap="round"/>
+<path id="Vector" d="M24.9229 54.2276C19.2716 52.1456 17.3631 45.1807 17.1152 41.9585V29.1559C17.1152 21.4424 23.1077 15.1895 30.4998 15.1895C37.8918 15.1895 43.8843 21.4424 43.8843 29.1559V39.7277C43.8843 43.4238 40.8881 46.42 37.192 46.42C33.496 46.42 30.4998 43.4238 30.4998 39.7277V30.0611" stroke={color} stroke-width="2" stroke-linecap="round"/>
+<path id="Ellipse 4" d="M36.4491 38.9844V28.5742C36.4491 25.2888 33.7858 22.6255 30.5004 22.6255C27.2151 22.6255 24.5518 25.2888 24.5518 28.5742C24.5518 28.5742 24.5518 33.4075 24.5518 40.0997C24.5518 46.792 30.5004 53.856 39.7952 53.856" stroke={color} stroke-width="2" stroke-linecap="round"/>
+</g>
+</svg>
