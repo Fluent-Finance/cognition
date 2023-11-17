cc520272333bc53e506e023f140fc53d50529e0e
⚡️0xO0O0⚡️ <757859+0xO0O0@users.noreply.github.com>
2023-11-17 09:49:35 +0000

    dev: cleaned up dashboard design

--- src/routes/dashboard/fluent.svelte
+++ src/routes/dashboard/fluent.svelte
@@ -4,6 +4,7 @@
   import { P }         from 'flowbite-svelte';
   import { Section }   from 'flowbite-svelte-blocks';
   import LineGraph     from '$components/LineGraph.svelte';
+  import PieGraph      from '$components/PieGraph.svelte';
   import BarGraph      from '$components/BarGraph.svelte';
   import DoughnutGraph from '$components/DoughnutGraph.svelte';
 
@@ -31,13 +32,21 @@
       <span class="text-base-50 dark:text-base-800">USPlus</span> Bank Reserves
     </div>
 
-    <div class='grid grid-cols-4 grid-flow-row gap-10'>
-      <div class='col-span-full row-span-3 md:col-span-2 lg:col-span-2'>
-        <DoughnutGraph /> 
-      </div>
-      <div class='col-span-full row-span-2 md:col-span-2'>
-        <BarGraph /> 
+    <div class='grid grid-cols-4 grid-flow-row gap-18'>
+      <div class='col-span-full row-span-2 lg:col-span-4 mb-20'>
         <LineGraph /> 
+      </div>
+
+      <div class='col-span-full row-span-2 lg:col-span-4 mb-40'>
+        <BarGraph /> 
+      </div>
+
+      <div class='items-center content-center col-span-full xs:col-span-2 row-span-1'>
+        <PieGraph /> 
+      </div>
+
+      <div class='col-span-full xs:col-span-2 row-span-1'>
+        <DoughnutGraph /> 
       </div>
     </div>
 
@@ -55,65 +64,20 @@
         font-normal
         text-center
         items-center
+        leading-loose
         my-10
         mx-0
         xs:mx-10
         md:mx-20
         p-2
         xs:p-10">
-          At Fluent Finance, we’re driven by a simple philosophy: put the users first. 
-          We’ve always believed the power of DeFi should be harnessed for the benefit 
-          of those who contribute to its growth. That’s why we’re thrilled to announce 
-          our integration with Flow Rewards 
           <a 
             class="text-primary dark:text-base-800" 
-            href="https://docs.google.com/document/d/11j66k-nuOCo1MXjSKzvt25-WX8jMjxxPepfbFS5HGVs/edit" 
+            href="https://github.com/Fluent-Finance" 
             target="_blank"> 
               open-sourced smart contracts.
           </a> 
         </P>
-      </div> 
-    </div> 
-
-    <div class={`
-    w-full
-    my-0
-    sm:my-40
-    h-[136px]
-    ${hTextBreaks}
-    font-semibold
-    font-basier
-    leading-[72px]
-    text-center
-    text-black
-    dark:text-white`}>
-      <span class="">USPlus</span>
-      <span class="text-base-50 dark:text-base-800">Defi</span>
-      <span class="">Pools</span>
-    </div>
-
-    <div class='grid grid-cols-4 grid-flow-col gap-10'>
-      <div class='col-span-full md:col-span-2 row-span-1 mb-10'>
-
-        <div class="
-        flex
-        mx-0
-        px-0
-        p-0">
-          <LineGraph /> 
-        </div>
-      </div>
-
-      <div class='col-span-full md:col-span-2 row-span-1 mb-10'>
-        <div class="
-        flex
-        mx-0
-        px-0
-        md:mx-0
-        md:px-0">
-          <BarGraph /> 
-        </div>
-
       </div> 
     </div> 
   </div> 
